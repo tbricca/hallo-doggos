67ed80509a436c310960eee5c2c311a7d241c64f
jbadan <jenna.badanowski@gmail.com>
2017-10-19 16:52:24 -0700

    edited routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Quiz from './Quiz';
 import Result from './Result';
-import Checkout from './Checkout';
-import Billing from './Billing';
 import Confirmation from './Confirmation';
 import NotFound from './NotFound';
 
@@ -27,8 +25,6 @@ class App extends Component {
           <Switch>
            <Route exact path="/" component={Quiz} />
            <Route path="/result" component={Result} />
-           <Route path="/checkout" component={Checkout} />
-           <Route path="/checkout/billing" component={Billing} />
            <Route path="/confirmation" component={Confirmation} />
            <Route component={NotFound} />
          </Switch>
