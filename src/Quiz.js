9bb20655bb1de7681faaece2bdf9e3740e9c6228
jbadan <jenna.badanowski@gmail.com>
2017-10-19 15:33:06 -0700

    form

---
+++ src/Quiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Quiz extends Component {
+  render() {
+    return (
+      <div className="App">
+        <form>
+        </form>
+      </div>
+    );
+  }
+}
+export default Quiz;
