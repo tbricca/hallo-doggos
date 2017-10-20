7587ff8789de34dbef851d504c79ac85e2f62951
jbadan <jenna.badanowski@gmail.com>
2017-10-19 18:01:48 -0700

    added navbar and fixed some general issues

---
+++ src/Confirmation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Confirmation extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+export default Confirmation;
---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>put 404 error page here </h1>
+      </div>
+    );
+  }
+}
+export default NotFound;
